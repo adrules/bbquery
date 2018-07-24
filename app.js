27cbd92660c8eed175d119a1d1e38ea4a0b615e9
Adrián Moreno <adrianmn90@gmail.com>
2018-07-24 20:51:52 +0200

    db.config implemented

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 const app = express();
 
+require('./configs/db.config');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
