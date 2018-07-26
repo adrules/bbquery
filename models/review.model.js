d7c6eba027ea66691aeb12ebd278410e02dad50a
Rulo Alonso <raul.alonso@sngular.team>
2018-07-26 20:00:57 +0200

    add: review model fixed:#6

---
+++ models/review.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const reviewModel = new mongoose.Schema({
+  userReviewer: {
+    type: { type: Schema.Types.ObjectId, ref: 'User' },
+    required: true
+  },
+  userReviewed: {
+    type: { type: Schema.Types.ObjectId, ref: 'Bbq' },
+    required: true
+  },
+  rate: {
+    type: Number,
+    min: [1, 'At least 1 star'],
+    max: [5, 'Too much stars'],
+    required: 'You must give a rate to the user',
+    validate: {
+      validator: Number.isInteger,
+      message: '{VALUE} is not an integer value'
+    }
+  },
+  message: {
+    type: String,
+    required: 'You must leave a message to the user'
+  }
+}, { timestamps: true });
+
+const Review = mongoose.model('Review', reviewSquema);
+
+module.exports = Review;
