c68218a2111b732eec9ef6517cd607386d6a2814
Adrián Moreno <adrianmn90@gmail.com>
2018-07-26 20:04:53 +0200

    login controller cleaned fixes#22

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -14,41 +14,5 @@ module.exports.create = (req, res, next) => {
 
 module.exports.doCreate = (req, res, next) => {
   console.log("doCreat has to be implemented");
-  // res.redirect('/users/create');
-
-  /*
-  const newUser = new User(req.body);
-
-  console.log(`REQ BODY: ${JSON.stringify(req.body)}`);
-
-  User.findOne({ email: newUser.email })
-    .then((user) => {
-      if (user) {
-        return res.render('users/create', {
-          user: newUser,
-          errors: {
-            email: "already exists"
-          }
-        });       
-      } else {
-        newUser.save()
-          .then((user) => {
-            res.redirect("/");
-          })
-          .catch((error) => {
-            if (error instanceof mongoose.Error.ValidationError) {
-              res.render('users/create', { 
-                user: newUser,
-                errors: error.errors
-              });
-            } else {
-              next(error);
-            }
-          });
-      }
-    })
-    .catch((error) => {
-      next(error);
-    });
-    */
+  res.redirect('/users/create');
 }
