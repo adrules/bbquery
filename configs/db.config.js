452adbd09b619d2407aecb4e29c00c510fb95cce
Adrián Moreno <adrianmn90@gmail.com>
2018-08-04 16:03:35 +0200

    Deleted DB_NAME from db_config

--- configs/db.config.js
+++ configs/db.config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log(`Connected to ${DB_NAME} database.`);
+    console.log(`Connected to bbquery database.`);
   }).catch((error) => {
     console.error(`Database connection error: ${error}`);
   });
