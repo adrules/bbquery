f3ed1f43f421fbd9564fa1a5a22dccdd01f2fc3c
Adrián Moreno <adrian@adrian.Home>
2018-08-06 06:47:03 +0200

    Fixed user seed adding default token and default active true

--- bin/seed-users.js
+++ bin/seed-users.js
@@ -16,7 +16,9 @@ for (let i = -1; ++i < numUsers;) {
     password: password,
     image: password,
     age: 25,
-    genre: 'female'
+    genre: 'female',
+    active: true,
+    token: '2fixdi6u8v9ekcu3ix6u1'
   }).save()
     .then(user => {
       console.log(`${user.firstName} correctly added to the collection`);
